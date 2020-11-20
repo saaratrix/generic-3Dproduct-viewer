b40bb0cfe83d9a67bccdc03fe82c710cfe4d8064
saaratrix <saaratrix@gmail.com>
2020-11-20 19:47:03 +0200

    Added selected mesh highlighting, added [title] as tooltip and allow camera to zoom in a bit closer.

--- src/app/3D/SelectedProductHighlighter.ts
+++ src/app/3D/SelectedProductHighlighter.ts
@@ -1,12 +1,7 @@
 import { ProductConfiguratorService } from "../product-configurator.service";
 import { ProductConfigurationEvent } from "../product-configurator-events";
 import { Subscription } from "rxjs";
-import {
-  Mesh,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer
-} from "three";
+import { Mesh, PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { OutlineEffect } from "three/examples/jsm/effects/OutlineEffect";
 
 
@@ -15,16 +10,28 @@ export class SelectedProductHighlighter {
   private subscriptions: Subscription[] = [];
 
   private isHovering: boolean = false;
-  public outlineEffect!: OutlineEffect;
+  private selectedMesh: Mesh | undefined;
+
+  private selectedEffect!: OutlineEffect;
+  private hoverEffect!: OutlineEffect;
 
   constructor(renderer: WebGLRenderer, private productConfiguratorService: ProductConfiguratorService) {
-    this.createOutlineEffect(renderer);
+    this.createOutlineEffects(renderer);
     this.subscriptions.push(
       this.productConfiguratorService.getSubject<Mesh>(ProductConfigurationEvent.Mesh_PointerEnter).subscribe((mesh) => {
         this.setHoverMaterial(mesh);
       }),
       this.productConfiguratorService.getSubject<Mesh>(ProductConfigurationEvent.Mesh_PointerLeave).subscribe((mesh) => {
         this.clearHoverMaterial(mesh);
+      }),
+      // Selection
+      this.productConfiguratorService.getSubject<Mesh>(ProductConfigurationEvent.Mesh_Selected).subscribe(mesh => {
+        this.selectedMesh = mesh;
+        this.setSelectedMaterial(mesh);
+      }),
+      this.productConfiguratorService.getSubject<Mesh>(ProductConfigurationEvent.Mesh_Deselected).subscribe(mesh => {
+        this.clearSelectedMaterial(mesh);
+        this.selectedMesh = undefined;
       }),
     );
   }
@@ -33,31 +40,52 @@ export class SelectedProductHighlighter {
     this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 
-  public renderOutline(scene: Scene, camera: PerspectiveCamera) {
-    if (!this.isHovering) {
-      return;
+  public renderOutline(scene: Scene, camera: PerspectiveCamera): void {
+    if (this.isHovering) {
+      camera.layers.set(1);
+      this.hoverEffect.renderOutline(scene, camera);
     }
 
-    camera.layers.set(1);
-    this.outlineEffect.renderOutline(scene, camera);
+    if (this.selectedMesh) {
+      camera.layers.set(2);
+      this.selectedEffect.renderOutline(scene, camera);
+    }
+
     camera.layers.set(0);
   }
 
-  private createOutlineEffect(renderer: WebGLRenderer): void {
-    this.outlineEffect = new OutlineEffect(renderer, {
+  private createOutlineEffects(renderer: WebGLRenderer): void {
+    const defaultThickness = 0.005; // default: 0.003
+    this.selectedEffect = new OutlineEffect(renderer, {
+      // snow
+      defaultColor: [ 255 / 255, 250 / 255, 250 / 255 ],
+      defaultThickness,
+    });
+
+    this.hoverEffect = new OutlineEffect(renderer, {
       // #b591c7
       defaultColor: [181 / 255,   145 / 255,   199 / 255],  // default: [0, 0, 0],
-      defaultThickness: 0.005,                              // default: 0.003
+      defaultThickness,
     });
   }
 
-  private setHoverMaterial(mesh: Mesh) {
+  private setSelectedMaterial(mesh: Mesh): void {
+    mesh.layers.enable(2);
+  }
+
+  private clearSelectedMaterial(mesh: Mesh): void {
+    mesh.layers.disable(2);
+  }
+
+  private setHoverMaterial(mesh: Mesh): void {
     this.isHovering = true;
     mesh.layers.enable(1);
   }
 
-  private clearHoverMaterial(mesh: Mesh) {
+  private clearHoverMaterial(mesh: Mesh): void {
     this.isHovering = false;
     mesh.layers.disable(1);
   }
+
+
 }
