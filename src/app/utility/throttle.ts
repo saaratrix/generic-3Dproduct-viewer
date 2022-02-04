7ad5e678d351e6e7b7e7935a6c53d0c41b3c2940
saaratrix <saaratrix@gmail.com>
2022-02-05 00:05:16 +0200

    34: Fixed types issue with throttle method. Did I not build it or what few months ago...? :D

--- src/app/utility/throttle.ts
+++ src/app/utility/throttle.ts
@@ -2,13 +2,14 @@
  * Throttles a method so it's only called once every interval.
  * @param callback Must be a lambda or a bound method for now so that "this" is correct.
  */
-export function throttle(callback: (...args: unknown[]) => void, interval: number): (...args: any[]) => void {
-  let isThrottling: boolean
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function throttle(callback: (...args: any[]) => void, interval: number): (...args: unknown[]) => void {
+  let isThrottling: boolean;
   let wasCalled: boolean = false;
 
   // We need to store the lastArgs so we always execute the last callback when throttling.
   let lastArgs: unknown[];
-  return function(...args: unknown[]): void {
+  return function (...args: unknown[]): void {
     lastArgs = args;
     if (isThrottling) {
       wasCalled = true;
