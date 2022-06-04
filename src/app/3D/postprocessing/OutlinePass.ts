e8a665879c6ee415a7981989b1f432e9e151ac99
saaratrix <saaratrix@gmail.com>
2022-06-04 22:53:56 +0300

    Improved the outline code!

--- src/app/3D/postprocessing/OutlinePass.ts
+++ src/app/3D/postprocessing/OutlinePass.ts
@@ -1,9 +1,14 @@
 import { FullScreenQuad, Pass } from "three/examples/jsm/postprocessing/Pass";
 import {
-  Camera, DoubleSide,
-  MeshBasicMaterial, NoBlending,
+  Camera,
+  Color,
+  DoubleSide,
+  MeshBasicMaterial,
+  NoBlending,
   RGBAFormat,
-  Scene, ShaderMaterial, UniformsUtils,
+  Scene,
+  ShaderMaterial,
+  UniformsUtils,
   Vector2,
   WebGLRenderer,
   WebGLRenderTarget,
@@ -13,23 +18,39 @@ import { CopyShader } from "three/examples/jsm/shaders/CopyShader";
 import { createSeperableBlurMaterial } from "./CreateBlurMaterial";
 import type { IUniform } from "three/src/renderers/shaders/UniformsLib";
 
+// The blur shader code is adapted from three.js' OutlinePass:
+// https://github.com/mrdoob/three.js/blob/dev/examples/jsm/postprocessing/OutlinePass.js
 export class OutlinePass extends Pass {
 
-  private hoverMaterial: MeshBasicMaterial;
-  private selectedMaterial: MeshBasicMaterial;
+  private readonly hoverMaterial: MeshBasicMaterial;
+  private readonly selectedMaterial: MeshBasicMaterial;
 
-  private maskRenderTarget: WebGLRenderTarget;
-  private blurRenderTarget: WebGLRenderTarget;
+  private readonly maskRenderTarget: WebGLRenderTarget;
+  /**
+   * This is the edge outline of the material.
+   */
+  private blurRenderTarget!: WebGLRenderTarget;
+  /**
+   * This is the glowing part of the outline.
+   */
+  private blurRenderHalfTarget!: WebGLRenderTarget;
 
-  private outlineMaterial: ShaderMaterial;
+  private readonly outlineMaterial: ShaderMaterial;
 
-  private blurMaterial: ShaderMaterial;
+  private blurMaterial!: ShaderMaterial;
+  private blurHalfMaterial!: ShaderMaterial;
+
   private blurHorizontalDirection = new Vector2(1, 0);
   private blurVerticalDirection = new Vector2(0, 1);
 
   private fsQuad: FullScreenQuad;
-  private copyUniforms: Record<string, IUniform>;
-  private materialCopy: ShaderMaterial;
+  private readonly copyUniforms: Record<string, IUniform>;
+  private readonly materialCopy: ShaderMaterial;
+
+  private downsampleResolution: number = 2;
+
+  private readonly edgeThickness: number = 1;
+  private readonly edgeGlow: number = 2;
 
   constructor(resolution: Vector2, private scene: Scene, private camera: Camera) {
     super();
@@ -50,53 +71,75 @@ export class OutlinePass extends Pass {
     this.maskRenderTarget = new WebGLRenderTarget(resolution.x, resolution.y, renderTargetOptions);
     this.maskRenderTarget.texture.name = "OutlinePass.mask";
     this.maskRenderTarget.texture.generateMipmaps = false;
-    this.blurRenderTarget = new WebGLRenderTarget(resolution.x, resolution.y, renderTargetOptions);
-    this.blurRenderTarget.texture.name = "OutlinePass.blur";
-    this.blurRenderTarget.texture.generateMipmaps = false;
 
     this.fsQuad = new FullScreenQuad();
 
     this.outlineMaterial = this.createOutlineMaterial();
-    this.blurMaterial = createSeperableBlurMaterial(4);
-    this.blurMaterial.uniforms.texSize.value.set(resolution.x, resolution.y);
-    this.blurMaterial.uniforms.kernelRadius.value = 4;
-
+    this.initBlurRenderTargetsAndMaterials(resolution, renderTargetOptions);
     // copy material
-    if ( CopyShader === undefined ) {
+    if (CopyShader === undefined) {
       console.error("THREE.OutlinePass relies on CopyShader");
     }
 
-    const copyShader = CopyShader;
-
-    this.copyUniforms = UniformsUtils.clone( copyShader.uniforms );
+    this.copyUniforms = UniformsUtils.clone(CopyShader.uniforms);
     this.copyUniforms.opacity.value = 1.0;
 
-    this.materialCopy = new ShaderMaterial( {
+    this.materialCopy = new ShaderMaterial({
       uniforms: this.copyUniforms,
-      vertexShader: copyShader.vertexShader,
-      fragmentShader: copyShader.fragmentShader,
+      vertexShader: CopyShader.vertexShader,
+      fragmentShader: CopyShader.fragmentShader,
       blending: NoBlending,
       depthTest: false,
       depthWrite: false,
       transparent: true,
-    } );
+    });
+  }
+
+  private initBlurRenderTargetsAndMaterials(resolution: Vector2, renderTargetOptions: WebGLRenderTargetOptions): void {
+    let resX = Math.round(resolution.x / this.downsampleResolution);
+    let resY = Math.round(resolution.y / this.downsampleResolution);
+
+    this.blurRenderTarget = new WebGLRenderTarget(resX, resY, renderTargetOptions);
+    this.blurRenderTarget.texture.name = "OutlinePass.blur";
+    this.blurRenderTarget.texture.generateMipmaps = false;
+
+    this.blurMaterial = createSeperableBlurMaterial(this.edgeThickness);
+    this.blurMaterial.uniforms.texSize.value.set(resX, resY);
+    this.blurMaterial.uniforms.kernelRadius.value = 1;
+
+    resX = Math.round(resX / 2);
+    resY = Math.round(resY / 2);
+
+    this.blurRenderHalfTarget = new WebGLRenderTarget(resX, resY, renderTargetOptions);
+    this.blurRenderHalfTarget.texture.name = "OutlinePass.blur.half";
+    this.blurRenderHalfTarget.texture.generateMipmaps = false;
+
+    this.blurHalfMaterial = createSeperableBlurMaterial(this.edgeGlow);
+    this.blurHalfMaterial.uniforms.texSize.value.set(resX, resY);
+    this.blurHalfMaterial.uniforms.kernelRadius.value = this.edgeGlow;
   }
 
   public render(renderer: WebGLRenderer, writeBuffer: WebGLRenderTarget, readBuffer: WebGLRenderTarget, deltaTime: number, maskActive: boolean): void {
     const oldAutoClear = renderer.autoClear;
+    const oldClearAlpha = renderer.getClearAlpha();
+    const oldClearColor = renderer.getClearColor(new Color());
+    renderer.setClearColor(new Color(0x000000), 0);
     // Since we're rendering multiple times to a render target we don't want to auto clear.
     renderer.autoClear = false;
     this.renderMaskTexture(renderer);
     this.renderBlurTexture(renderer);
+    this.renderBlurHalfTexture(renderer);
 
-    this.scene.overrideMaterial = null;
-    renderer.setRenderTarget(readBuffer);
+    // This adds the outline material on top of the previous render.
     this.fsQuad.material = this.outlineMaterial;
     this.outlineMaterial.uniforms.maskTexture.value = this.maskRenderTarget.texture;
     this.outlineMaterial.uniforms.blurTexture.value = this.blurRenderTarget.texture;
+    this.outlineMaterial.uniforms.blurHalfTexture.value = this.blurRenderHalfTarget.texture;
+    renderer.setRenderTarget(readBuffer);
     this.fsQuad.render(renderer);
 
     renderer.autoClear = oldAutoClear;
+    renderer.setClearColor(oldClearColor, oldClearAlpha);
 
     if (this.renderToScreen) {
       this.fsQuad.material = this.materialCopy;
@@ -115,11 +158,14 @@ export class OutlinePass extends Pass {
 
     this.scene.overrideMaterial = this.hoverMaterial;
     this.camera.layers.set(1);
-
     renderer.render(this.scene, this.camera);
+
     this.scene.overrideMaterial = this.selectedMaterial;
     this.camera.layers.set(2);
     renderer.render(this.scene, this.camera);
+
+    this.camera.layers.set(0);
+    this.scene.overrideMaterial = null;
   }
 
   private renderBlurTexture(renderer: WebGLRenderer): void {
@@ -136,8 +182,22 @@ export class OutlinePass extends Pass {
     this.fsQuad.render(renderer);
   }
 
+  private renderBlurHalfTexture(renderer: WebGLRenderer): void {
+    renderer.setRenderTarget(this.blurRenderHalfTarget);
+    renderer.clear();
+
+    this.fsQuad.material = this.blurHalfMaterial;
+    this.blurHalfMaterial.uniforms.colorTexture.value = this.maskRenderTarget.texture;
+    this.blurHalfMaterial.uniforms.direction.value = this.blurHorizontalDirection;
+    this.fsQuad.render(renderer);
+
+    this.blurHalfMaterial.uniforms.colorTexture.value = this.blurRenderHalfTarget.texture;
+    this.blurHalfMaterial.uniforms.direction.value = this.blurVerticalDirection;
+    this.fsQuad.render(renderer);
+  }
+
   private createOutlineMaterial(): ShaderMaterial {
-    const material = new ShaderMaterial({
+    return new ShaderMaterial({
       vertexShader:
         `varying vec2 vUv;
         void main() {
@@ -149,6 +209,7 @@ export class OutlinePass extends Pass {
         `varying vec2 vUv;
         uniform sampler2D maskTexture;
 				uniform sampler2D blurTexture;
+				uniform sampler2D blurHalfTexture;
 
 				vec4 getOutlineColor(float mask, float blur, vec3 outlineColor)
         {
@@ -159,17 +220,21 @@ export class OutlinePass extends Pass {
 				void main() {
 					vec4 maskColor = texture2D(maskTexture, vUv);
 					vec4 blurColor = texture2D(blurTexture, vUv);
-					vec4 hoverColor = getOutlineColor(maskColor.r, blurColor.r, vec3(1.0, 1.0, 1.0));
+					vec4 blurHalfColor = texture2D(blurHalfTexture, vUv);
+
+          float blurAlpha = min(ceil(blurColor.w + blurHalfColor.w), 1.0);
+					vec3 combinedBlur = blurAlpha * min(blurColor.xyz + blurHalfColor.xyz, vec3(1.0, 1.0, 1.0));
+
+					vec4 hoverColor = getOutlineColor(maskColor.r, combinedBlur.r, vec3(1.0, 1.0, 1.0));
 					vec4 finalColor = vec4(blurColor.xyz - maskColor.xyz, hoverColor.w);
 					gl_FragColor = hoverColor;
 				}`,
       uniforms: {
         maskTexture: { value: null },
         blurTexture: { value: null },
+        blurHalfTexture: { value: null },
       },
       transparent: true,
     });
-
-    return material;
   }
 }
