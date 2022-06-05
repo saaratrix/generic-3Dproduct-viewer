543855bb3fe7d0198ebf321dca5503e5ccb0d79c
saaratrix <saaratrix@gmail.com>
2022-06-05 12:47:41 +0300

    34: Solved issue using window width & height instead of the element's width & height.

--- src/app/3D/PointerEventHandler.ts
+++ src/app/3D/PointerEventHandler.ts
@@ -123,8 +123,8 @@ export class PointerEventHandler {
 
   private setPointerPosition(event: PointerEvent | MouseEvent): void {
     // Convert pointer XY to screen space.
-    this.pointerPosition.x = (event.clientX / window.innerWidth) * 2 - 1;
-    this.pointerPosition.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    this.pointerPosition.x = (event.clientX / this.element.offsetWidth) * 2 - 1;
+    this.pointerPosition.y = -(event.clientY / this.element.offsetHeight) * 2 + 1;
   }
 
   private getIntersections(event: PointerEvent | MouseEvent): Intersection[] {
